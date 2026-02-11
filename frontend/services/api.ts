9e64f728a8230b0ba2244370d73059a68a337b36
gastondieud <gastondieud@gmail.com>
2026-02-11 10:47:44 +0000

    still fixing back end front connection error

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = 'https://linkcontact.onrender.com/api/';
+const API_BASE_URL = 'https://linkcontact.onrender.com/';
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL
 });
